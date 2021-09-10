a94d587e670e5c6aab5d47d777464727b5e63659
Juan Carlos Orantes <72782842+Orantes-J@users.noreply.github.com>
2021-09-09 23:28:38 -0700

    Zip Method used to concatenate two list items while iteration

--- algo_list.py
+++ algo_list.py
@@ -53,3 +53,15 @@ print(new_list1)
 
 
 print(new_words)
+
+# CONCATENATE WITH ZIP METHOD
+
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+
+x = zip(list1, list2[::-1])
+
+print(tuple(x))
